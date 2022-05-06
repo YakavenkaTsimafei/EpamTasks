a434732b062c9ecfa6e2cf0b08276577130b4bed
YakavenkaTsimafei <akovenkotimofej9@gmail.com>
2022-05-06 22:55:33 +0300

    beginning classes2_1

---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+import by.epam.lab.Material;
+import by.epam.lab.Subject;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Material material = new Material("steel", 7850);
+
+        Subject subject = new Subject("wire", material, 0.03);
+        System.out.println(subject);
+        material.setName("cooper");
+        material.setDensity(8500);
+        System.out.println(subject);
+    }
+}
---
+++ src/by/epam/lab/Material.java
@@ -0,0 +1,38 @@
+package by.epam.lab;
+
+public class Material {
+    private String name;
+    private int density;
+
+    public Material() {
+
+    }
+
+    public Material(String name, int density) {
+        this.name = name;
+        this.density = density;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDensity() {
+        return density;
+    }
+
+    public void setDensity(int density) {
+        this.density = density;
+    }
+
+    @Override
+    public String toString() {
+        return
+                name + ";" +
+                        +density;
+    }
+}
