8678ac30d9101e06709c14267f3c3be7a741faa2
YakavenkaTsimafei <akovenkotimofej9@gmail.com>
2022-12-15 19:17:38 +0300

    final0.1 generics1_3

--- src/Runner.java
+++ src/Runner.java
@@ -3,15 +3,15 @@ import by.epam.lab.bean.*;
 
 public class Runner {
     public static void main(String[] args) {
-        Purchase<Sale, Number> p1 = new Purchase<>(new Product("Milk", new Byn(170)), 20);
-        PurchaseUtils<Sale, Number> pu1 = new PurchaseUtils<>(p1);
+        Purchase<Sale, Integer> p1 = new Purchase<>(new Product("Milk", new Byn(170)), 20);
+        PurchaseUtils<Sale, Integer> pu1 = new PurchaseUtils<>(p1);
         System.out.println(pu1.printPurchase());
         System.out.println(pu1.printCost());
-        Purchase<Sale, Number> p2 = new Purchase<>(new Product("Sugar", new Byn(300)), 12.5);
-        PurchaseUtils<Sale, Number> pu2 = new PurchaseUtils<>(p2);
+        Purchase<Sale, Double> p2 = new Purchase<>(new Product("Sugar", new Byn(300)), 12.5);
+        PurchaseUtils<Sale, Double> pu2 = new PurchaseUtils<>(p2);
         System.out.println(pu2.printCost());
         pu2.printCostDiff(p1);
-        Purchase<Sale, Number> p3 = new Purchase<>(new DiscountProduct("Sugar", new Byn(280), new Byn(10)), 60);
+        Purchase<DiscountProduct, Integer> p3 = new Purchase<>(new DiscountProduct("Sugar", new Byn(280), new Byn(10)), 60);
         PurchaseUtils<Service, Double> pu4 = new PurchaseUtils<>(new Purchase<>(new Service("Gym", new Byn(7560), 5), 2.25));
         Purchase<Service, Double> p4 = pu4.getPurchase();
         Service service = pu4.getPurchase().getProduct();
